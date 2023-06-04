b9ffeb512c005f0e1d6da14fb25110ea1cd6ac07
harish242 <120125264+harish242@users.noreply.github.com>
2023-06-04 14:04:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,25 @@ import React, { useState } from "react";
 import "../styles/App.css";
 
 export default function App() {
+    const [theme,setTheme]=useState("light")
+    const HandleonChange=(event)=>{
+        setTheme((prev)=>prev==="light"?"dark":"light")
+    }
     return (
         <div className="App">
             <h1>Newton School</h1>
             <div>
                 <form>
                     <label>UserName</label>
-                    <input></input>
+                    <input type="text"></input>
                     <label>Password</label>
-                    <input></input>
+                    <input type="password"></input>
                     <button>Login</button>
                 </form>
             </div>
             <label> {theme === "light" ? "Light Mode" : "Dark Mode"}</label>
-            <input type="checkbox" id="switch" />
-            <label for="switch" className="label">
+            <input type="checkbox" id="switch" onChange={HandleonChange}/>
+            <label htmlFor="switch" className="label">
                 Toggle
             </label>
         </div>
